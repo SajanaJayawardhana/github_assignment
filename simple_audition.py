6764ab80daa4fad49be82312871f2c09cfab89b7
SajanaJayawardhana <your.email@example.com>
2026-01-29 14:39:07 +0530

    Added python script and README file

---
+++ simple_audition.py
@@ -0,0 +1,11 @@
+# simple_addition.py
+# This program adds two numbers and displays the result
+
+def add_numbers(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = add_numbers(num1, num2)
+print("Sum =", result)
